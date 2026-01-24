14a7d82b694e036396e37015a027b5fad21e2806
phucnguyen <nguyen098xx@gmail.com>
2026-01-24 09:45:01 +0700

    Add DummyRenderObject and update MainScene loading
    Introduced DummyRenderObject for rendering a red rectangle and logging screen size. Expanded MainScene's LoadObjects method for improved object management and added new using directives for engine and entities.

--- src/Ascendance.Sandbox/Rendering/Scenes/MainScene.cs
+++ src/Ascendance.Sandbox/Rendering/Scenes/MainScene.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) 2025 PPN Corporation. All rights reserved.
 
 using Ascendance.Rendering.Attributes;
+using Ascendance.Rendering.Engine;
+using Ascendance.Rendering.Entities;
 using Ascendance.Rendering.Scenes;
 using Ascendance.Rendering.UI.Indicators;
 
@@ -18,5 +20,27 @@ internal sealed class MainScene : BaseScene
         _overlay.Show();
     }
 
-    protected override void LoadObjects() => base.AddObject(_overlay);//AssetManager.Instance.LoadTexture(GraphicsConfig.AssetRoot + "");//Button button = new("OK", );
+    public class DummyRenderObject : RenderObject
+    {
+        private SFML.Graphics.RectangleShape _rect;
+        public DummyRenderObject()
+        {
+            System.Console.WriteLine($"Khởi tạo DummyRect — ScreenSize: {GraphicsEngine.ScreenSize.X},{GraphicsEngine.ScreenSize.Y}");
+            _rect = new SFML.Graphics.RectangleShape(new SFML.System.Vector2f(100, 100))
+            {
+                FillColor = SFML.Graphics.Color.Red
+            };
+        }
+        protected override SFML.Graphics.Drawable GetDrawable() => _rect;
+    }
+
+    protected override void LoadObjects()
+    {
+        //System.Console.WriteLine("=== LoadObjects của MainScene đang được gọi ===");
+
+        base.AddObject(_overlay);
+        //System.Console.WriteLine("Đã Add DummyRect vào scene!");
+        //AssetManager.Instance.LoadTexture(GraphicsConfig.AssetRoot + "");//Button button = new("OK", );
+        _overlay.Show();
+    }
 }
