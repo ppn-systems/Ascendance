06b76e6a224a5cbdb821397786ff7e798aa621c1
phucnguyen <nguyen098xx@gmail.com>
2026-02-08 00:00:58 +0700

    Add AppConfig for DI setup; update refs and add Program.cs
    Added static AppConfig class to configure dependency injection for ILogger and IPacketCatalog, registering key packet types. Updated project references to use Ascendance.Infrastructure instead of Ascendance.Security. Introduced a new Program.cs with an empty Program class in Ascendance.Hosting.

---
+++ src/Ascendance.Hosting/AppConfig.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) 2026 Ascendance Team. All rights reserved.
+
+using Ascendance.Shared.Protocol;
+using Nalix.Common.Diagnostics;
+using Nalix.Common.Messaging.Packets.Abstractions;
+using Nalix.Framework.Injection;
+using Nalix.Logging;
+using Nalix.Shared.Messaging.Catalog;
+
+namespace Ascendance.Hosting;
+
+public static class AppConfig
+{
+    static AppConfig()
+    {
+        if (InstanceManager.Instance.GetExistingInstance<ILogger>() == null)
+        {
+            InstanceManager.Instance.Register<ILogger>(NLogix.Host.Instance);
+        }
+
+        // 1) Build packet catalog.
+        PacketCatalogFactory factory = new();
+
+        // REGISTER packets here (single source of truth).
+        _ = factory.RegisterPacket<ResponsePacket>();
+        _ = factory.RegisterPacket<CredentialsPacket>();
+        //_ = factory.RegisterPacket<CredsUpdatePacket>();
+
+        IPacketCatalog catalog = factory.CreateCatalog();
+
+        // 2) Expose catalog through your current service locator.
+        InstanceManager.Instance.Register<IPacketCatalog>(catalog);
+    }
+}
---
+++ src/Ascendance.Hosting/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ascendance.Hosting;
+
+internal class Program
+{
+}
