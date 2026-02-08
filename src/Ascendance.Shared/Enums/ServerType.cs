6b532ef681e98a352144e21b433666d1df07c24c
phucnguyen <nguyen098xx@gmail.com>
2026-02-08 21:12:00 +0700

    Add ServerType enum for domestic and global server types
    Introduced ServerType.cs in Ascendance.Shared.Enums namespace.
    Defines Domestic and Global server types with documentation.
    Includes copyright and XML comments for clarity.

---
+++ src/Ascendance.Shared/Enums/ServerType.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2025 PPN Corporation. All rights reserved.
+
+namespace Ascendance.Shared.Enums;
+
+/// <summary>
+/// Represents the type of game server.
+/// </summary>
+public enum ServerType
+{
+    /// <summary>
+    /// Domestic server with normal game progression.
+    /// </summary>
+    Domestic,
+
+    /// <summary>
+    /// Global server connecting players worldwide.
+    /// </summary>
+    Global
+}
