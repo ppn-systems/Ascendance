41ae9b53d0c0accc2bb282de7ca363b9036f891a
phucnguyen <nguyen098xx@gmail.com>
2026-02-09 12:33:28 +0700

    Persist credentials on login submit in LoginScene
    Added event handler for SubmitRequested in LoginScene to save user credentials using Credentials.Save. Introduced dependency on Ascendance.Desktop.Services. No existing functionality was removed.

--- src/Ascendance.Desktop/Scenes/Login/LoginScene.cs
+++ src/Ascendance.Desktop/Scenes/Login/LoginScene.cs
@@ -2,6 +2,7 @@
 
 using Ascendance.Desktop.Scenes.Login.View;
 using Ascendance.Desktop.Scenes.ServerSelection.View;
+using Ascendance.Desktop.Services;
 using Ascendance.Rendering.Attributes;
 using Ascendance.Rendering.Engine;
 using Ascendance.Rendering.Scenes;
@@ -27,6 +28,11 @@ public sealed class LoginScene : BaseScene
         loginView.BackRequested += () =>
             SceneManager.Instance.ScheduleSceneChange(
                 ConfigurationManager.Instance.Get<GraphicsConfig>().MainScene);
+
+        loginView.SubmitRequested += () =>
+        {
+            Credentials.Save(loginView.Username, loginView.Password);
+        };
 
         base.AddObject(loginView);
         base.AddObject(background);
