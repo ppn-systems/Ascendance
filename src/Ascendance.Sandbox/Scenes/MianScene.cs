b0b8d1f73f6b15bae7c1041f0311103b78518d32
phucnguyen <nguyen098xx@gmail.com>
2026-02-10 19:14:30 +0700

    Add MainScene class and update project structure
    Removed Scenes folder reference from csproj. Added MainScene.cs with dynamic loading, player and tile map setup, collision configuration, and camera initialization for game scene.

---
+++ src/Ascendance.Sandbox/Scenes/MianScene.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) 2025 PPN Corporation. All rights reserved.
+
+using Ascendance.Domain.Players;
+using Ascendance.Domain.Tiles;
+using Ascendance.Rendering.Attributes;
+using Ascendance.Rendering.Camera;
+using Ascendance.Rendering.Engine;
+using Ascendance.Rendering.Managers;
+using Ascendance.Rendering.Scenes;
+using Nalix.Framework.Configuration;
+using SFML.Graphics;
+using SFML.System;
+
+namespace Ascendance.Sandbox.Scenes;
+
+[DynamicLoad]
+public sealed class MainScene : BaseScene
+{
+    public MainScene() : base(ConfigurationManager.Instance.Get<GraphicsConfig>().MainScene)
+    {
+    }
+
+    protected override void LoadObjects()
+    {
+        // 1. Load texture nguyên sheet (KHÔNG cần cắt thủ công)
+        Texture playerTexture = AssetManager.Instance.LoadTexture("res/texture/characters/1.png");
+
+        // 2. Tạo Player - AnimationController tự động cắt sprite bên trong
+        Player player = new(playerTexture, new Vector2f(400, 300));
+
+        // 3. Setup tile map collision
+        TileMap tileMap = TmxMapLoader.Load("res/maps/1.tmx");
+        player.TileMap = tileMap;
+        player.CollisionLayerName = "Collision";
+
+        // 4. Setup camera
+        Camera2D.Instance.Reset(new Vector2f(400, 300), new Vector2f(800, 600));
+        player.Camera = Camera2D.Instance;
+
+        base.AddObject(player);
+        base.AddObject(tileMap);
+    }
+}
